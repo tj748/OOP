2896abac3c77bbd4465aff507ed49cf371d1aab7
tj748 <40781862+tj748@users.noreply.github.com>
2018-08-04 13:07:24 -0400

    First commit

---
+++ tokens.h
@@ -0,0 +1,34 @@
+// Assignment 7
+
+const char invalid = 'i';
+const char name = 'a';
+
+const char number = '8';
+const char quit = 'q';
+const char print = ';';
+const char power = '^';
+
+const string prompt = "> ";
+const string result = "= ";
+
+
+class Token {
+public:
+	char kind;
+	double value;
+	string name;
+	Token(char ch) : kind{ ch } {}
+	Token(char ch, double val) : kind{ ch }, value{ val } {}
+	Token(char ch, string n) : kind{ ch }, name{ n } {}
+};
+
+class Token_stream {
+public:
+	Token get();
+	void putback(Token t);
+	Token popback();
+	void ignore(char c);
+
+private:
+	vector<Token> buffer;
+};
