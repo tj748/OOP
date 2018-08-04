763e160aa63300311809ee468be0e268a70a9010
tj748 <40781862+tj748@users.noreply.github.com>
2018-08-04 13:09:46 -0400

    First commit

---
+++ vars_test.cpp
@@ -0,0 +1,26 @@
+// Assignment 7
+
+#include "std_lib_facilities.h"
+#include "vars.h"
+
+/*
+int main() {
+
+	set_value("blue", 185.2);
+	set_value("green", 36.7);
+	set_value("red", 94.05);
+	set_value("yellow", 54.2);
+	set_value("blue", 200.2);
+
+	double d;
+	d = get_value("blue");
+	cout << "blue = " << d << endl;
+	d = get_value("green");
+	cout << "green = " << d << endl;
+	d = get_value("red");
+	cout << "red = " << d << endl;
+	d = get_value("yellow");
+	cout << "yellow = " << d << endl;
+
+}
+*/
