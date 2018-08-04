ab66e345e1cb8694ec620da51cd19a0fd5a4ec0d
tj748 <40781862+tj748@users.noreply.github.com>
2018-08-04 13:07:54 -0400

    First commit

---
+++ tokens.cpp
@@ -0,0 +1,68 @@
+// Assignment 7
+
+#include "std_lib_facilities.h"
+#include "tokens.h"
+
+void Token_stream::putback(Token t) {
+	buffer.push_back(t);
+}
+
+Token Token_stream::popback() {
+	Token t = buffer.back();
+	buffer.pop_back();
+	return t;
+}
+
+Token Token_stream::get() {
+	if (buffer.size() > 0) {
+		Token t = popback();
+		return t;
+	}
+
+	char ch;
+	cin >> ch;
+
+	switch (ch) {
+	case print: case quit: case '(': case '+': case '-': case '*': case '/': case '%': case ')': case '=': case power: 
+		return Token{ ch };
+
+	case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.':
+		cin.putback(ch);
+		double val;
+		cin >> val;
+		return Token{ number, val };
+
+	default:
+		if (isalpha(ch)) {
+			string s;
+			s += ch;
+			while (cin.get(ch) && isalpha(ch)) {
+				s += ch;
+			}
+			cin.putback(ch);
+			return Token{ name, s };
+		}
+		return Token{ invalid, double(ch) };
+	}
+	return Token{ quit };
+}
+
+
+void Token_stream::ignore(char c) {
+	while (buffer.size() > 0) {
+		Token t = popback();
+		if (t.kind == c) {
+			return;
+		}
+	}
+
+	char ch = 0;
+	while (cin >> ch) {
+		if (ch == c) {
+			return;
+		}
+	}
+}
+
+
+
